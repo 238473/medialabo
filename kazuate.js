cd2b5be247845903ed87c14228adfa223046f1d2
G384732023 <238473@st.takushoku-u.ac.jp>
2024-05-16 17:46:40 +0900

    kadai

---
+++ kazuate.js
@@ -0,0 +1,94 @@
+// 答え
+let kotae = Math.floor(Math.random()*10) + 1;
+console.log('答え（デバッグ用）: ' + kotae);
+
+// 入力回数（予想回数）
+let kaisu = 0;
+let yoso = 4;
+
+// 予想を4回実行する
+// 将来以下の hantei(); の4回の呼び出しを全て削除する
+// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
+hantei();console.log((kaisu+1)+'回目の予想:'+yoso);
+if(yoso===kotae){
+  console.log('正解です.おめでとう！');
+}else if(yoso<kotae){
+  console.log('まちがい.答えはもっと大きいですよ');
+}else if(yoso>kotae){
+  console.log('まちがい.答えはもっと小さいですよ');
+};
+hantei();console.log((kaisu+2)+'回目の予想:'+yoso);
+if(yoso===kotae){
+  console.log('答えは 4でした.すでにゲームは終わっています');
+}else if(yoso<kotae){
+  console.log('まちがい.答えはもっと大きいですよ');
+}else if(yoso>kotae){
+  console.log('まちがい.答えはもっと小さいですよ');
+}
+hantei();console.log((kaisu+3)+'回目の予想:'+yoso);
+
+if(yoso===kotae){
+  console.log('答えは 4 でした．すでにゲームは終わっています');
+}else {
+  console.log('まちがい.残念でした答えは '+kotae+" です.");
+}
+hantei();console.log((kaisu+4)+'回目の予想:'+yoso);
+if(yoso===kotae){
+  console.log('答えは 4 でした．すでにゲームは終わっています');
+}else {
+  console.log("答えは "+kotae+" でした.すでにゲームは終わっています");
+}
+
+// ボタンを押した後の処理をする関数 hantei() の定義
+function hantei() {
+  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+  let yoso = 4;
+  
+  // 課題3-1: 正解判定する
+  // kotae と yoso が一致するかどうか調べて結果を出力
+  // 課題3-1における出力先はコンソール
+  
+if(yoso===kotae){
+  console.log((kaisu+4)+'回目の予想:'+yoso);
+  console.log('正解です.おめでとう！');
+}else if(yoso<kotae){
+  console.log((kaisu+1)+'回目の予想:'+yoso);
+  console.log('まちがい.答えはもっと大きいですよ');
+}else if(yoso>kotae){
+  console.log((kaisu+1)+'回目の予想:'+yoso);
+  console.log('まちがい.答えはもっと小さいですよ');
+};
+
+if(yoso===kotae){
+  console.log((kaisu+5)+'回目の予想:'+yoso);
+
+  console.log('答えは 4でした.すでにゲームは終わっています');
+}else if(yoso<kotae){
+  console.log((kaisu+2)+'回目の予想:'+yoso);
+
+  console.log('まちがい.答えはもっと大きいですよ');
+}else if(yoso>kotae){
+  console.log((kaisu+2)+'回目の予想:'+yoso);
+
+  console.log('まちがい.答えはもっと小さいですよ');
+}
+if(yoso===kotae){
+  console.log((kaisu+6)+'回目の予想:'+yoso);
+
+  console.log('答えは 4 でした．すでにゲームは終わっています');
+}else {
+  console.log((kaisu+3)+'回目の予想:'+yoso);
+
+  console.log('まちがい.残念でした答えは '+kotae+" です.");
+}
+  if(yoso===kotae){
+    console.log((kaisu+7)+'回目の予想:'+yoso);
+
+    console.log('答えは 4 でした．すでにゲームは終わっています');
+  }else {
+    console.log((kaisu+4)+'回目の予想:'+yoso);
+
+    console.log("答えは "+kotae+" でした.すでにゲームは終わっています");
+  }
+
+}
